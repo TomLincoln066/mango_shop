ae80500088e7c2b1cdd6cffb5e3d8e3b32a5983b
Will Lo <darthhun54@gmail.com>
2024-07-25 09:12:04 +0800

    Product CRUD Create Products done

---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,28 @@
+﻿using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Mango.Web.Models;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> list = new List<ProductDto>();
+            var response = await _productService.GetAllProductsAsync<ResponseDto>();
+            list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+            return View(list);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+    }
+}
